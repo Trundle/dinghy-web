87af232828b69c0018019c1ffbd4065c7558e6c4
Vincent Haupert <mail@vincent-haupert.de>
2022-08-22 13:53:20 +0200

    Use dinghy from nixpkgs

--- default.nix
+++ default.nix
@@ -1,43 +1,12 @@
 { buildPythonPackage
-, aiofiles
 , aiohttp
 , chameleon
-, click-log
-, emoji
-, fetchFromGitHub
-, glom
+, dinghy
 , hatchling
-, jinja2
 , lib
-, pyyaml
 , setuptools
 }:
 
-let
-  dinghy = buildPythonPackage rec {
-    pname = "dinghy";
-    version = "0.13.2";
-
-    src = fetchFromGitHub {
-      owner = "nedbat";
-      repo = pname;
-      rev = version;
-      sha256 = "sha256-uRiWcrs3xIb6zxNg0d6/+NCqnEgadHSTLpS53CoZ5so=";
-    };
-
-    propagatedBuildInputs = [
-      aiofiles
-      aiohttp
-      click-log
-      emoji
-      glom
-      jinja2
-      pyyaml
-    ];
-
-    pythonImportsCheck = [ "dinghy.cli" ];
-  };
-in
 buildPythonPackage {
   pname = "dinghy-web";
   version = "2022.8.1";
